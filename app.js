7e7530ec2d9703da7964bc16feb29652223dff00
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-24 17:20:38 +0800

    code refactored

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
-const menuDropdown = document.querySelector(".menu-dropdown");
-const backgroundBlur = document.querySelector(".background-blur");
+const menuButton = document.querySelector(".menu-icon");
 
-document.addEventListener("click", (event) => {
-	const menu = event.target.matches(".menu-icon");
+const closeButton = document.querySelector(".close-icon");
 
-	if (menu) {
-		menuDropdown.classList.add("active");
-		backgroundBlur.classList.add("active");
+const menuDropdown = document.querySelector(".menu-group");
+
+const bgBlur = document.querySelector(".background-blur");
+
+const buttons = document.querySelectorAll("button");
+
+const illustration = document.querySelector(".illu");
+
+let dropdownActive = false;
+
+const onDrop = () => {
+	bgBlur.classList.add("active");
+	menuDropdown.classList.add("active");
+	dropdownActive = true;
+};
+
+const offDrop = () => {
+	bgBlur.classList.remove("active");
+	menuDropdown.classList.remove("active");
+	dropdownActive = false;
+};
+
+menuButton.onclick = () => onDrop();
+
+closeButton.onclick = () => offDrop();
+
+menuDropdown.addEventListener("click", (e) => {
+	const item = e.target.matches(".menu-item-name");
+
+	if (item) {
+		e.target.closest(".menu-item").classList.toggle("active");
 	}
+});
 
-	const closeIcon = event.target.matches(".close-icon");
+for (const btn of buttons) btn.addEventListener("click", (e) => e.preventDefault());
 
-	if (closeIcon) {
-		menuDropdown.classList.remove("active");
-		backgroundBlur.classList.remove("active");
-	}
-
-	const itemList = event.target.matches(".itemBy");
-
-	let currentItemList;
-
-	if (itemList) {
-		currentItemList = event.target.closest(".menu-item");
-		currentItemList.classList.toggle("active");
+window.addEventListener("resize", (e) => {
+	if (window.innerWidth > 756) {
+		illustration.src = "/images/image-hero-desktop.png";
+	} else {
+		illustration.src = "/images/image-hero-mobile.png";
 	}
 });
